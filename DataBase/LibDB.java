8ac2c0f44b41a43f95fe88589700989a022690dc
JAEMOON-LEEE <ljmm5721@naver.com>
2025-10-23 17:33:36 +0900

    주석 처리

--- DataBase/LibDB.java
+++ DataBase/LibDB.java
@@ -36,10 +36,10 @@ public class LibDB<T extends DB_Element>
      * @return     찾은 객체 또는 null
      */
     public T findElement(String id) {
-        Iterator<T> iterator = db.iterator();
-        while (iterator.hasNext()) {
+        Iterator<T> iterator = db.iterator(); // 7장 Iterator (p423) 참조, db에 저장된 걸렉션에서 요소를 순차 검색할때 사용
+        while (iterator.hasNext()) { //7장 Iterator (p423) 참조, hasNext()메소드를 통해 다음 방문할 요소가 있으면 true retrun
             T elem = iterator.next();
-            if(elem.getID().equals(id)){
+            if(elem.getID().equals(id)){ //6장 boolean equals(Object obj) 잠조 (p356), 두 객체의 내용이 같은지 비교하는 메소드
                 return elem;
             }
         }
